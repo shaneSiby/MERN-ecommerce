94ad8a968d19b30e1bd0c5dbc5d07f463b2f981e
shaneSiby <shanesiby@gmail.com>
2024-04-11 11:10:34 +0530

    completed

---
+++ frontend/src/Pages/Product.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
+import ProductDisplay from '../Components/ProductDispaly/ProductDisplay'
+import Breadcrum from '../Components/Breadcrum/Breadcrum'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../RelatedProducts/RelatedProducts'
+
+const Product = () => {
+  const {all_product} = useContext(ShopContext)
+  const{productId} = useParams();
+  const product = all_product.find((e)=>{
+   return e.id === Number(productId);
+  })
+  return (
+    <div>
+    <Breadcrum product={product}/>
+    <ProductDisplay product={product}/>
+    <DescriptionBox/>
+    <RelatedProducts/>
+    </div>
+  )
+}
+
+export default Product
+
+
+
+
